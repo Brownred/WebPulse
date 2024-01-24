c9801f4c3f07b247fe22b9246074e17d0dd851b8
Lenox <lenoxmiheso@gmail.com>
2024-01-24 14:11:09 +0300

    modified:   WebPulseSolutions/settings.py

--- WebPulseSolutions/settings.py
+++ WebPulseSolutions/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY") #'django-insecure--4t0ie06%=jjgsa53=8*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split()
 
 
 # Application definition
